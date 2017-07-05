0f70a0a4aca858b8ea5cfae0948b77491ff7d3c8
Geison Biazus <geisonbiazus@gmail.com>
2017-07-04 21:45:53 -0300

    Cursor Affordance

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+	[SerializeField]
+	Texture2D walkCursor = null;
+
+	[SerializeField]
+	Texture2D targetCursor = null;
+
+	[SerializeField]
+	Texture2D unknownCursor = null;
+
+	[SerializeField]
+	Vector2 cursorHotspot = new Vector2(96, 96);
+
+	CameraRaycaster raycaster;
+
+	// Use this for initialization
+	void Start () {
+		raycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		Texture2D cursor;
+		switch (raycaster.layerHit)
+		{
+			case Layer.Walkable:
+				cursor = walkCursor;
+				break;
+			case Layer.Enemy:
+				cursor = targetCursor;
+				break;
+			case Layer.RaycastEndStop:
+				cursor = unknownCursor;
+				break;
+			default:
+				Debug.LogError("We don't know what cursor to show");
+				return;
+		}
+		Cursor.SetCursor(cursor, cursorHotspot, CursorMode.Auto);
+	}
+}
